012bacd38b7150ca65ea120086ec989126d1ed97
r1N0Xmk2 <rene7297152@gmail.com>
2014-08-31 21:43:01 +0800

    Remove excess
    Streamline code

--- Initial HTML/scripts/common.js
+++ Initial HTML/scripts/common.js
@@ -1,22 +1,4 @@
 $(function(){
-	var href=window.location.href;
-	var hrefs=href.split('.')
-	if(hrefs[hrefs.length-1]=="htm"){
-		var hrefss=hrefs[hrefs.length-2].split('/');
-		var num=parseInt(hrefss[hrefss.length-1]);
-		if(num!=NaN) 
-		$('a.ymjc').attr('href', '/error/error_report.jsp?info_id='+num+'&tm_id=177')
-		.css('visibility', 'visible');;
-	}
-	
+
 })
 
-var href=window.location.href;
-	var hrefs=href.split('.')
-	if(hrefs[hrefs.length-1]=="htm"){
-		var hrefss=hrefs[hrefs.length-2].split('/');
-		var num=parseInt(hrefss[hrefss.length-1]);
-		if(num.isNaN()) alert('nan');
-		else if(num)alert('aaaa')
-		
-	}
